71959d237848458864d4567ae9ebc2dde86990ca
jundhd <jundihd@student.telkomuniversity.ac.id>
2024-11-21 18:59:52 +0700

    Initial Commit

---
+++ SG-Motion/NewView.swift
@@ -0,0 +1,18 @@
+//
+//  NewView.swift
+//  SG-Motion
+//
+//  Created by Jundi HD on 21/11/24.
+//
+
+import SwiftUI
+
+struct NewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewView()
+}
---
+++ SG-Motion/SG_MotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SG_MotionApp.swift
+//  SG-Motion
+//
+//  Created by Jundi HD on 21/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SG_MotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
